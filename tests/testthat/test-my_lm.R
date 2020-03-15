423b5e5ca792ab8ca713b382bcea4180e1f08c5f
Dairong Han <61256311+NauticaSunwind@users.noreply.github.com>
2020-03-15 02:18:44 -0700

    Delete test-my_lm.R

--- tests/testthat/test-my_lm.R
+++
@@ -1,8 +0,0 @@
-test_that("the output is a table", {
-  expect_is(my_lm(formula = lifeExp ~ gdpPercap + continent,
-                  data = my_gapminder), "table")
-})
-
-test_that("my_lm shows an error message when the input is incorrect", {
-  expect_error(my_lm("string"))
-})
