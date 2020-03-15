5ad7bea3afed04e220f6e29d1a063ffb1b2d7102
Dairong Han <61256311+NauticaSunwind@users.noreply.github.com>
2020-03-15 02:19:00 -0700

    Delete test-my_rf_cv.R

--- tests/testthat/test-my_rf_cv.R
+++
@@ -1,9 +0,0 @@
-data("iris")
-
-test_that("the output is numeric", {
-  expect_is(my_rf_cv(k = 5), "numeric")
-})
-
-test_that(" my_rf_cv shows an error message when the input is non-numeric", {
-  expect_error(my_rf_cv("FGO"))
-})
