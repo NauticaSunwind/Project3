6f316af66b9ee8c95f1fc30dab7d583eba6ce4ab
Dairong Han <61256311+NauticaSunwind@users.noreply.github.com>
2020-03-15 14:31:38 -0700

    Add files via upload

---
+++ R/my_rf_cv.R
@@ -0,0 +1,42 @@
+#' Random Forest Cross Validation Function
+#'
+#' This function predicts the class value cl by k-fold Cross-validation
+#'          and random forest algorithms
+#' @param k integer of the number of folds
+#' @keywords prediction
+#'
+#' @return Numeric number of the CV mean squared error
+#'
+#' @examples
+#' my_rf_cv(5)
+#'
+#' @import class randomForest stringr tidyverse dplyr
+#'
+#' @export
+my_rf_cv <- function(k) {
+  # check if input is numeric, if not, stop and show error information
+  if (!is.numeric(k)) {
+    stop("input must be numeric")
+  }
+  my_gapminder <- my_gapminder
+  l <- nrow(my_gapminder)
+  # split data in k parts, randomly
+  fold <- sample(rep(1:k, length = l))
+  data <- my_gapminder %>% select(lifeExp, gdpPercap)
+  data[, "split"] <- fold
+
+  mse <- c()
+  for (i in 1:k) {
+    # define the training data as all the data not in the ith fold
+    data_train <- data %>% dplyr::filter(split != i)
+    data_test <- data %>% dplyr::filter(split == i)
+    # train a random forest model with 50 trees to predict lifeExp using covariate gdpPercap
+    my_model <- randomForest(lifeExp ~ gdpPercap, data = data_train, ntree = 50)
+    # predict the lifeExp of the ith fold which was used as test data
+    my_pred <- predict(my_model, select(data_test, -split))
+    # evaluate the MSE, the average squared difference between predicted lifeExp and true lifeExp
+    mse <- c(mse, mean((my_pred - data_test$lifeExp)^2))
+  }
+  output <- mean(mse)
+  return(output)
+}
